c29a3d66fe3398180f4adf7bdba43f31eb844137
feliperbdantas <feliperbdantas@outlook.com>
2025-09-13 19:21:18 -0300

    feat: section 4 - creating our project in spring initializr

---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/feliperbdantas/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.feliperbdantas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
