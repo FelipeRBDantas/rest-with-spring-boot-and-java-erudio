2ec0bea7dc130618233bfc284c1b4c9756a0d51e
feliperbdantas <feliperbdantas@outlook.com>
2025-09-13 22:37:49 -0300

    feat: section 4 - creating the first rest endpoint

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/controllers/GreetingController.java
@@ -0,0 +1,20 @@
+package br.com.feliperbdantas.controllers;
+
+import br.com.feliperbdantas.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String path) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, path));
+    }
+}
