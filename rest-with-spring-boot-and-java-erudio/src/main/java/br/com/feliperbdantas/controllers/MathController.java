5c6ac6af0604903e3707b8b009e8a7ab32e39e43
feliperbdantas <feliperbdantas@outlook.com>
2025-09-13 23:28:38 -0300

    feat: section 4 - defining the logic of our restful api

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/controllers/MathController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/controllers/MathController.java
@@ -7,13 +7,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/math")
 public class MathController {
-
-
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(
             @PathVariable("numberOne") String numberOne,
             @PathVariable("numberTwo") String numberTwo
-    ) {
+    ) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalArgumentException();
+
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String numberOne) {
         return 1D;
+    }
+
+    private boolean isNumeric(String numberOne) {
+        return true;
     }
 }
