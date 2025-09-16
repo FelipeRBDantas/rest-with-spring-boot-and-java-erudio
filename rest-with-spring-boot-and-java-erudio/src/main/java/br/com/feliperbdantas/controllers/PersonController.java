39ffc41809c15070ec2643c214e40dfeef4f478f
feliperbdantas <feliperbdantas@outlook.com>
2025-09-16 13:28:59 -0300

    feat: section 4 - dependency injection with @Service and @Autowired

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/controllers/PersonController.java
@@ -1,11 +1,26 @@
 package br.com.feliperbdantas.controllers;
 
+import br.com.feliperbdantas.model.Person;
 import br.com.feliperbdantas.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/person")
 public class PersonController {
     @Autowired
     private PersonServices personServices;
+
+    @RequestMapping(
+            value = "/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person findById(@PathVariable("id") String id) {
+        return personServices.findById(id);
+    }
 }
--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/services/PersonServices.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/services/PersonServices.java
@@ -1,10 +1,27 @@
 package br.com.feliperbdantas.services;
 
+import br.com.feliperbdantas.model.Person;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
+
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public Person findById(String id) {
+        logger.info("Finding one Person!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Felipe");
+        person.setLastName("Dantas");
+        person.setAddress("São Paulo - SP - Brasil");
+        person.setGender("Male");
+
+        return person;
+    }
 }
