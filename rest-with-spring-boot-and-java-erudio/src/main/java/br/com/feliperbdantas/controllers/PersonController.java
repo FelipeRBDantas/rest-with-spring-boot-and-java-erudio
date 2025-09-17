71245879cc93cab877fd49280f59cf6d2b3a00fb
feliperbdantas <feliperbdantas@outlook.com>
2025-09-17 08:09:34 -0300

    feat: section 4 - refactoring the people controller

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/controllers/PersonController.java
@@ -4,6 +4,7 @@ import br.com.feliperbdantas.model.Person;
 import br.com.feliperbdantas.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,25 +15,22 @@ public class PersonController {
     @Autowired
     private PersonServices service;
 
-    @RequestMapping(
-            method = RequestMethod.GET,
+    @GetMapping(
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     public List<Person> findAll() {
         return service.findAll();
     }
 
-    @RequestMapping(
+    @GetMapping(
             value = "/{id}",
-            method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     public Person findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
-    @RequestMapping(
-            method = RequestMethod.POST,
+    @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
@@ -41,8 +39,7 @@ public class PersonController {
     }
 
 
-    @RequestMapping(
-            method = RequestMethod.PUT,
+    @PutMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
@@ -50,11 +47,10 @@ public class PersonController {
         return service.update(person);
     }
 
-    @RequestMapping(
-            value = "/{id}",
-            method = RequestMethod.DELETE
-    )
-    public void delete(@PathVariable("id") Long id) {
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
         service.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/services/PersonServices.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/services/PersonServices.java
@@ -7,13 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
-    private final AtomicLong counter = new AtomicLong();
-
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
     @Autowired
