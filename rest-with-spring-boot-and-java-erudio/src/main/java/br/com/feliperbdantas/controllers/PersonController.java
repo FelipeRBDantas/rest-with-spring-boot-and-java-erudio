0a5a66a16901adef78b4b8277dfed9fe0a579b56
feliperbdantas <feliperbdantas@outlook.com>
2025-09-17 17:59:59 -0300

    feat: section 4 - adjusting the service and the controller

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/controllers/PersonController.java
@@ -1,6 +1,6 @@
 package br.com.feliperbdantas.controllers;
 
-import br.com.feliperbdantas.model.Person;
+import br.com.feliperbdantas.data.dto.PersonDTO;
 import br.com.feliperbdantas.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -18,7 +18,7 @@ public class PersonController {
     @GetMapping(
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public List<Person> findAll() {
+    public List<PersonDTO> findAll() {
         return service.findAll();
     }
 
@@ -26,7 +26,7 @@ public class PersonController {
             value = "/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public Person findById(@PathVariable("id") Long id) {
+    public PersonDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
@@ -34,7 +34,7 @@ public class PersonController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public Person create(@RequestBody Person person) {
+    public PersonDTO create(@RequestBody PersonDTO person) {
         return service.create(person);
     }
 
@@ -43,7 +43,7 @@ public class PersonController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public Person update(@RequestBody Person person) {
+    public PersonDTO update(@RequestBody PersonDTO person) {
         return service.update(person);
     }
 
--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/services/PersonServices.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/services/PersonServices.java
@@ -1,6 +1,9 @@
 package br.com.feliperbdantas.services;
 
+import br.com.feliperbdantas.data.dto.PersonDTO;
 import br.com.feliperbdantas.exception.ResourceNotFoundException;
+import static br.com.feliperbdantas.mapper.ObjectMapper.parseObject;
+import static br.com.feliperbdantas.mapper.ObjectMapper.parseListObjects;
 import br.com.feliperbdantas.model.Person;
 import br.com.feliperbdantas.repository.PersonRepository;
 import org.slf4j.Logger;
@@ -17,25 +20,29 @@ public class PersonServices {
     @Autowired
     PersonRepository repository;
 
-    public List<Person> findAll() {
+    public List<PersonDTO> findAll() {
         logger.info("Finding all People.");
 
-        return repository.findAll();
+        return parseListObjects(repository.findAll(), PersonDTO.class);
     }
 
-    public Person findById(Long id) {
+    public PersonDTO findById(Long id) {
         logger.info("Finding one Person.");
 
-        return repository.findById(id)
+        var entity = repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("No Records found for this ID."));
+
+        return parseObject(entity, PersonDTO.class);
     }
 
-    public Person create(Person person) {
+    public PersonDTO create(PersonDTO person) {
         logger.info("Creating one Person.");
 
-        return repository.save(person);
+        var entity = parseObject(person, Person.class);
+
+        return parseObject(repository.save(entity), PersonDTO.class);
     }
-    public Person update(Person person) {
+    public PersonDTO update(PersonDTO person) {
         logger.info("Updating one Person.");
 
         Person entity = repository.findById(person.getId())
@@ -46,7 +53,7 @@ public class PersonServices {
         entity.setAddress(person.getAddress());
         entity.setGender(person.getGender());
 
-        return repository.save(person);
+        return parseObject(repository.save(entity), PersonDTO.class);
     }
 
     public void delete(Long id) {
