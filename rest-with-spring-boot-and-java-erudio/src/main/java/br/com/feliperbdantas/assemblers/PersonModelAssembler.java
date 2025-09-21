adfb22ffbc90ba41008bc126f20402c5f285cd40
feliperbdantas <feliperbdantas@outlook.com>
2025-09-21 13:45:52 -0300

    feat: section 13 - implementing hateoas links for all verbs (get, post, put and delete) / refactoring with model assembler

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/assemblers/PersonModelAssembler.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/feliperbdantas/assemblers/PersonModelAssembler.java
@@ -2,7 +2,6 @@ package br.com.feliperbdantas.assemblers;
 
 import br.com.feliperbdantas.controllers.v1.PersonController;
 import br.com.feliperbdantas.data.dto.v1.PersonDTO;
-import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
@@ -11,17 +10,17 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @Component
 public class PersonModelAssembler implements RepresentationModelAssembler<PersonDTO, EntityModel<PersonDTO>> {
-
     @Override
     public EntityModel<PersonDTO> toModel(PersonDTO dto) {
         return EntityModel.of(dto,
+                linkTo(methodOn(PersonController.class).findAll()).withRel("findAll"),
                 linkTo(methodOn(PersonController.class).findById(dto.getId())).withSelfRel(),
-                linkTo(methodOn(PersonController.class).findAll()).withRel("persons")
+                linkTo(methodOn(PersonController.class).create(dto)).withRel("create"),
+                linkTo(methodOn(PersonController.class).update(dto)).withRel("update"),
+                linkTo(methodOn(PersonController.class).delete(dto.getId())).withRel("delete")
         );
     }
 
