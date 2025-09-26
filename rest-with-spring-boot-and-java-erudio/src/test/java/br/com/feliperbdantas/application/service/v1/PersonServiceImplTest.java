0783211a659f0731eb33494e8a3e159edb25c0b3
feliperbdantas <feliperbdantas@outlook.com>
2025-09-26 11:53:10 -0300

    test: service test

--- rest-with-spring-boot-and-java-erudio/src/test/java/br/com/feliperbdantas/application/service/v1/PersonServiceImplTest.java
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/feliperbdantas/application/service/v1/PersonServiceImplTest.java
@@ -1,6 +1,7 @@
 package br.com.feliperbdantas.application.service.v1;
 
 import br.com.feliperbdantas.application.dto.v1.PersonDTO;
+import br.com.feliperbdantas.application.presenter.v1.PersonPresenter;
 import br.com.feliperbdantas.exception.RequiredObjectIsNullException;
 import br.com.feliperbdantas.application.mapper.PersonMapper;
 import br.com.feliperbdantas.domain.model.Person;
@@ -15,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.hateoas.Link;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,6 +26,12 @@ class PersonServiceImplTest {
 
     @Mock
     PersonRepository repository;
+
+    @Mock
+    PersonMapper mapper;
+
+    @Mock
+    PersonPresenter presenter;
 
     @InjectMocks
     PersonServiceImpl service;
@@ -42,12 +50,136 @@ class PersonServiceImplTest {
 
     @Test
     void findById() {
+        Person person = input.mockEntity(1);
+        person.setId(1L);
 
+        PersonDTO personDTO = input.mockDTO(1);
+
+        String pathRoot = "http://localhost:8080";
+
+        PersonDTO personDTOWithHateoas = input.mockDTO(1);
+        personDTOWithHateoas.setId(1L);
+        personDTOWithHateoas.add(Link.of(pathRoot.concat("/api/v1/person/1")).withSelfRel());
+        personDTOWithHateoas.add(Link.of(pathRoot.concat("/api/v1/person")).withRel("findAll"));
+        personDTOWithHateoas.add(Link.of(pathRoot.concat("/api/v1/person")).withRel("create"));
+        personDTOWithHateoas.add(Link.of(pathRoot.concat("/api/v1/person")).withRel("update"));
+        personDTOWithHateoas.add(Link.of(pathRoot.concat("/api/v1/person/1")).withRel("delete"));
+
+        when(repository.findById(1L)).thenReturn(Optional.of(person));
+
+        when(mapper.toDto(person)).thenReturn(personDTO);
+
+        when(presenter.present(personDTO)).thenReturn(personDTOWithHateoas);
+
+        var result = service.findById(1L);
+
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertNotNull(result.getLinks());
+
+        assertTrue(
+                result.getLinks().stream()
+                        .anyMatch(link -> link.getRel().value().equals("self")
+                        && link.getHref().endsWith("/api/v1/person/1"))
+        );
+
+        assertTrue(
+                result.getLinks().stream()
+                        .anyMatch(link -> link.getRel().value().equals("findAll")
+                        && link.getHref().endsWith("/api/v1/person"))
+        );
+
+        assertTrue(
+                result.getLinks().stream()
+                        .anyMatch(link -> link.getRel().value().equals("create")
+                        && link.getHref().endsWith("/api/v1/person"))
+        );
+
+        assertTrue(
+                result.getLinks().stream()
+                        .anyMatch(link -> link.getRel().value().equals("update")
+                        && link.getHref().endsWith("/api/v1/person"))
+        );
+
+        assertTrue(
+                result.getLinks().stream()
+                        .anyMatch(link -> link.getRel().value().equals("delete")
+                        && link.getHref().endsWith("/api/v1/person/1"))
+        );
+
+        assertEquals("Address Test1", result.getAddress());
+        assertEquals("First Name Test1", result.getFirstName());
+        assertEquals("Last Name Test1", result.getLastName());
+        assertEquals("Female", result.getGender());
     }
 
     @Test
     void create() {
+        Person person = input.mockEntity(1);
 
+        Person persisted = person;
+        persisted.setId(1L);
+
+        PersonDTO personDTO = input.mockDTO(1);
+
+        String pathRoot = "http://localhost:8080";
+
+        PersonDTO personDTOWithHateoas = input.mockDTO(1);
+        personDTOWithHateoas.setId(1L);
+        personDTOWithHateoas.add(Link.of(pathRoot.concat("/api/v1/person/1")).withSelfRel());
+        personDTOWithHateoas.add(Link.of(pathRoot.concat("/api/v1/person")).withRel("findAll"));
+        personDTOWithHateoas.add(Link.of(pathRoot.concat("/api/v1/person")).withRel("create"));
+        personDTOWithHateoas.add(Link.of(pathRoot.concat("/api/v1/person")).withRel("update"));
+        personDTOWithHateoas.add(Link.of(pathRoot.concat("/api/v1/person/1")).withRel("delete"));
+
+        when(mapper.toEntity(personDTO)).thenReturn(person);
+
+        when(repository.save(person)).thenReturn(persisted);
+
+        when(mapper.toDto(person)).thenReturn(personDTO);
+
+        when(presenter.present(personDTO)).thenReturn(personDTOWithHateoas);
+
+        var result = service.create(personDTO);
+
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertNotNull(result.getLinks());
+
+        assertTrue(
+                result.getLinks().stream()
+                        .anyMatch(link -> link.getRel().value().equals("self")
+                                && link.getHref().endsWith("/api/v1/person/1"))
+        );
+
+        assertTrue(
+                result.getLinks().stream()
+                        .anyMatch(link -> link.getRel().value().equals("findAll")
+                                && link.getHref().endsWith("/api/v1/person"))
+        );
+
+        assertTrue(
+                result.getLinks().stream()
+                        .anyMatch(link -> link.getRel().value().equals("create")
+                                && link.getHref().endsWith("/api/v1/person"))
+        );
+
+        assertTrue(
+                result.getLinks().stream()
+                        .anyMatch(link -> link.getRel().value().equals("update")
+                                && link.getHref().endsWith("/api/v1/person"))
+        );
+
+        assertTrue(
+                result.getLinks().stream()
+                        .anyMatch(link -> link.getRel().value().equals("delete")
+                                && link.getHref().endsWith("/api/v1/person/1"))
+        );
+
+        assertEquals("Address Test1", result.getAddress());
+        assertEquals("First Name Test1", result.getFirstName());
+        assertEquals("Last Name Test1", result.getLastName());
+        assertEquals("Female", result.getGender());
     }
 
     @Test
